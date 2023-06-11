6d38b93e1b2f752312d9174de7d4939cffd19c11
evilpuppet <zevilpuppet@gmail.com>
2023-06-11 16:12:19 -0300

    add new icons

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ class Todo {
     }
   }
 
-  isDueSoon() {
+   isDueSoon() {
     const currentDate = new Date();
     const taskDate = new Date(this.date);
     const timeDiff = taskDate.getTime() - currentDate.getTime();
@@ -32,37 +32,30 @@ const uploadButton = document.getElementById('uploadButton');
 let todoIndex = 1;
 let todos = [];
 
-addTodoButton.addEventListener("click", () => {
+function createTodoElement(todo) {
   const li = document.createElement("li");
   li.className = "mb-2 relative";
   const createdDate = new Date().toLocaleString("en-US");
 
-  const todo = new Todo(`Task ${todoIndex}`, "");
-  todos.push(todo);
-
   li.innerHTML = `
-        <div class="flex items-center">
-          <input type="checkbox" class="mr-2">
-          <span class="todo-name">${todo.name}</span>
-          <input type="datetime-local" class="ml-4 todo-date">
-          <button class="ml-4 focus:outline-none toggle-button">
-            <svg class="h-4 w-4 text-gray-500 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-              <!-- SVG path for the toggle button icon -->
-            </svg>
-          </button>
-          <button class="ml-4 focus:outline-none remove-button">
-            <svg class="h-4 w-4 text-gray-500 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-              <!-- SVG path for the remove button icon -->
-            </svg>
-          </button>
-        </div>
-        <textarea class="hidden mt-2 ml-4 w-full px-3 py-2 bg-gray-100 border border-gray-300 rounded toggle-textarea"></textarea>
-        <div class="text-right">
-          <small class="ml-2 text-gray-500 toggle-date hidden">Created ${createdDate}</small>
-        </div>
-      `;
-  todoList.appendChild(li);
-  todoIndex++;
+    <div class="flex items-center w-full">
+      <input type="checkbox" class="mr-2">
+      <span class="todo-name">${todo.name}</span>
+      <div class="flex justify-end">
+        <input type="datetime-local" class="ml-4 todo-date">
+        <button id="arrowButton" class="ml-4 focus:outline-none toggle-button">
+        </button>
+        <button id="removeButton" class="ml-4 focus:outline-none remove-button">
+        </button>
+      </div>
+    </div>
+    <div class="mr-4">
+      <textarea class="hidden mt-2 ml-4 w-full px-3 py-2 bg-gray-100 border border-gray-300 rounded toggle-textarea"></textarea>
+      <div class="text-right">
+        <small class="ml-2 text-gray-500 toggle-date hidden">Created ${createdDate}</small>
+      </div>
+    </div>
+  `;
 
   const toggleButton = li.querySelector(".toggle-button");
   const toggleTextarea = li.querySelector(".toggle-textarea");
@@ -76,7 +69,8 @@ addTodoButton.addEventListener("click", () => {
     toggleTextarea.classList.toggle("hidden");
     toggleDate.classList.toggle("hidden");
     if (!toggleTextarea.classList.contains("hidden")) {
-      toggleTextarea.style.height = toggleTextarea.scrollHeight + "px";
+      console.log('toggleTextarea.scrollHeight', toggleTextarea.scrollHeight)
+      toggleTextarea.style.height = "100px";
     }
   });
 
@@ -87,7 +81,7 @@ addTodoButton.addEventListener("click", () => {
 
   todoName.addEventListener("blur", () => {
     todoName.contentEditable = false;
-    todo.name = todoName.textContent;
+    todo.name = todoName.textContent.trim();
     updateLocalStorage();
   });
 
@@ -125,20 +119,29 @@ addTodoButton.addEventListener("click", () => {
     checkDownloadButtonVisibility();
   });
 
+  return li;
+}
+
+function addTodo() {
+  const todo = new Todo(`Task ${todoIndex}`, "");
+  todos.push(todo);
+
+  const li = createTodoElement(todo);
+  todoList.appendChild(li);
+  todoIndex++;
+
   updateLocalStorage();
   checkDownloadButtonVisibility();
-});
+}
+
+addTodoButton.addEventListener("click", addTodo);
 
 function checkDownloadButtonVisibility() {
-  if (todoList.childElementCount > 0) {
-    downloadButton.classList.remove("hidden");
-  } else {
-    downloadButton.classList.add("hidden");
-  }
+  downloadButton.classList.toggle("hidden", todoList.childElementCount === 0);
 }
 
 function updateLocalStorage() {
-  localStorage.setItem("todos", JSON.stringify(todos));
+  // localStorage.setItem("todos", JSON.stringify(todos));
 }
 
 function loadTodosFromLocalStorage() {
@@ -146,42 +149,12 @@ function loadTodosFromLocalStorage() {
   if (savedTodos) {
     todos = JSON.parse(savedTodos);
     todos.forEach((todo) => {
-      const li = document.createElement("li");
-      li.className = "mb-2 relative";
-      const createdDate = new Date().toLocaleString("en-US");
-
-      li.innerHTML = `
-            <div class="flex items-center">
-              <input type="checkbox" class="mr-2">
-              <span class="todo-name">${todo.name}</span>
-              <input type="datetime-local" class="ml-4 todo-date">
-              <button class="ml-4 focus:outline-none toggle-button">
-                <svg class="h-4 w-4 text-gray-500 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                <path fill-rule="evenodd" d="M12 9a1 1 0 0 1 2 0v3a1 1 0 0 1-1.7.7l-2.5-2.5a1 1 0 0 1 0-1.4L12 9zm-6-5a1 1 0 1 1 2 0v10a1 1 0 0 1-2 0V4zm11.3 8.3a1 1 0 0 1-1.4 1.4l-2.5-2.5a1 1 0 0 1 0-1.4l2.5-2.5a1 1 0 0 1 1.4 1.4L15.42 9l2.3 2.3z" clip-rule="evenodd" />
-                </svg>
-              </button>
-              <button class="ml-4 focus:outline-none remove-button">
-                <svg class="h-4 w-4 text-gray-500 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                  <!-- SVG path for the remove button icon -->
-
-                  <path fill-rule="evenodd" d="M5 5a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v1h2a1 1 0 0 1 1 1v9a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h2V5zm3 9h2v2H8v-2zm0-6h2v4H8V8zm4 0h2v4h-2V8zm0 6h2v2h-2v-2z" clip-rule="evenodd" />
-                </svg>
-              </button>
-            </div>
-            <textarea class="hidden mt-2 ml-4 w-full px-3 py-2 bg-gray-100 border border-gray-300 rounded toggle-textarea"></textarea>
-            <div class="text-right">
-              <small class="ml-2 text-gray-500 toggle-date hidden">Created ${createdDate}</small>
-            </div>
-          `;
+      const li = createTodoElement(todo);
       todoList.appendChild(li);
 
-      const toggleButton = li.querySelector(".toggle-button");
-      const toggleTextarea = li.querySelector(".toggle-textarea");
-      const todoName = li.querySelector(".todo-name");
+      const checkbox = li.querySelector('input[type="checkbox"]');
       const todoDate = li.querySelector(".todo-date");
       const toggleDate = li.querySelector(".toggle-date");
-      const checkbox = li.querySelector('input[type="checkbox"]');
-      const removeButton = li.querySelector(".remove-button");
 
       if (todo.checked) {
         checkbox.checked = true;
@@ -189,76 +162,45 @@ function loadTodosFromLocalStorage() {
         toggleDate.textContent = `Finished ${todo.checkedDate}, Created ${todo.createdDate}`;
       }
 
-      toggleButton.addEventListener("click", () => {
-        toggleTextarea.classList.toggle("hidden");
-        toggleDate.classList.toggle("hidden");
-        if (!toggleTextarea.classList.contains("hidden")) {
-          toggleTextarea.style.height = toggleTextarea.scrollHeight + "px";
-        }
-      });
-
-      todoName.addEventListener("dblclick", () => {
-        todoName.contentEditable = true;
-        todoName.focus();
-      });
-
-      todoName.addEventListener("blur", () => {
-        todoName.contentEditable = false;
-        todo.name = todoName.textContent;
-        updateLocalStorage();
-      });
-
-      todoName.addEventListener("keydown", (event) => {
-        if (event.key === "Enter") {
-          event.preventDefault();
-          todoName.blur();
-        }
-      });
-
-      todoDate.addEventListener("change", () => {
-        todo.date = todoDate.value;
-        updateLocalStorage();
-        if (todo.isDueSoon()) {
-          alert(`Task '${todo.name}' is due in less than 3 hours!`);
-        }
-      });
-
-      checkbox.addEventListener("change", () => {
-        todo.toggleChecked();
-        if (checkbox.checked) {
-          todoName.classList.add("line-through");
-          toggleDate.textContent = `Finished ${todo.checkedDate}, Created ${todo.createdDate}`;
-        } else {
-          todoName.classList.remove("line-through");
-          toggleDate.textContent = `Created ${todo.createdDate}`;
-        }
-        updateLocalStorage();
-      });
-
-      removeButton.addEventListener("click", () => {
-        li.remove();
-        todos = todos.filter((t) => t !== todo);
-        updateLocalStorage();
-        checkDownloadButtonVisibility();
-      });
-
       todoDate.value = todo.date;
-    });
-
-    downloadButton.addEventListener("click", () => {
-      const jsonTodos = JSON.stringify(todos);
-      console.log("click download", todos);
-      const blob = new Blob([jsonTodos], { type: "application/json" });
-      const url = URL.createObjectURL(blob);
-      const link = document.createElement("a");
-      link.href = url;
-      link.download = "todos.json";
-      link.click();
-      URL.revokeObjectURL(url);
     });
 
     checkDownloadButtonVisibility();
   }
 }
 
-loadTodosFromLocalStorage();
+// loadTodosFromLocalStorage();
+
+downloadButton.addEventListener("click", () => {
+  const jsonTodos = JSON.stringify(todos);
+  const blob = new Blob([jsonTodos], { type: "application/json" });
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement("a");
+  link.href = url;
+  link.download = "todos.json";
+  link.click();
+  URL.revokeObjectURL(url);
+});
+
+uploadButton.addEventListener('change', (event) => {
+  const file = event.target.files[0];
+  const reader = new FileReader();
+  reader.onload = function (e) {
+    const contents = e.target.result;
+    try {
+      todos = JSON.parse(contents);
+      todoList.innerHTML = '';
+      todoIndex = 1;
+      todos.forEach((todo) => {
+        const li = createTodoElement(todo);
+        todoList.appendChild(li);
+        todoIndex++;
+      });
+      checkDownloadButtonVisibility();
+    } catch (error) {
+      console.error("Error parsing JSON file:", error);
+    }
+  };
+  reader.readAsText(file);
+});
+
